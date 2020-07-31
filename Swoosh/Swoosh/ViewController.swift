5723257887b3905571bb5d8a9da6c7d19b35d20b
Rafa Zaidan <rafayzaidanapple@gmail.com>
2020-07-31 14:14:22 +0200

    Created segues for the first two screens.

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // Creating the unwind segue
+    @IBAction func goBack(unwindSegue: UIStoryboardSegue) {
+        
+    }
 
 }
 
